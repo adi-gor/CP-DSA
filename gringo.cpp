a8bd392781eb06a1588a63b111fe0030d34a5f54
Aditya Gor <adityagor13@gmail.com>
2023-03-13 19:57:00 +0530

    Add files via upload

---
+++ gringo.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+const ll mod = 1e9 + 7;
+
+void solve()
+{
+
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+
+
+
+	int t;
+	cin >> t;
+	//t = 1;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
